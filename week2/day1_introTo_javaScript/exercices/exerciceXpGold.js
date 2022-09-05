f94242b75ef85221afbb5ca62241fac9008d5636
FloreLaure <floresdg60@gmail.com>
2022-09-05 09:14:56 +0000

    nouveau fichier : week2/day1_introTo_javaScript/exercices/exerciceXpGold.js 	nouveau fichier : week2/day1_introTo_javaScript/exercices/exerciceXpNinja.js

---
+++ week2/day1_introTo_javaScript/exercices/exerciceXpGold.js
@@ -0,0 +1,31 @@
+
+//exercice 1
+
+let sentence = ["my","favorite","color","is","blue"];
+console.log(sentence);
+
+
+//exercice 2
+
+let str1 = "mix";
+let str2 = "pod";
+
+let s1=str1.split("");
+let s2=str2.split("");
+s1.splice(0,2,"i","m");
+console.log(s1);
+s2.splice(0,2,"o","p");
+console.log(s2);
+
+let sum=str1+" "str2;
+console.log(sum);
+
+
+//exercice 3
+
+let num1=Number(prompt("entrer un nombre"));
+let num2=Number(prompt("entrer un deuxième nombre"));
+alert(num1+num2);
+
+
+
