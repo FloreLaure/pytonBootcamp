d6932275b374f42a61798071c50e52a13f37d53c
FloreLaure <floresdg60@gmail.com>
2022-09-12 12:16:54 +0000

    modifié :         week4/day3/exercices/exerciceXp.py

--- week4/day3/exercices/exerciceXp.py
+++ week4/day3/exercices/exerciceXp.py
@@ -1,7 +1,32 @@
 
 
-list1 = [5, 10, 15, 20, 25, 50, 20]
-if(20 in list1):
-    print(list1.index(20))
-    list1.index(20)=200
-    print(list1)
+## exercice1
+
+##  Convert the two following lists, into dictionaries.
+##  Hint: Use the zip method
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+dictionnaire=dict(zip(keys, values))
+print(dictionnaire)
+
+
+
+##  exercice2
+
+family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
+tolal=0
+for x, y in family.items():
+    if 0<=y<3:
+        print("c'est gratuit pour ",x)
+    elif 3<=y<=10:
+        print("le billet est de 10$ pour ",x)
+        total=tolal+10
+    else:
+        print("le billet est de 15$ pour ",x)
+        total=tolal+15
+
+print("le total est ",tolal)
+
+
+##  xercice3
