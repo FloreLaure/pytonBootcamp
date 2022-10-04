32599af0692d4c88f163cff4fef72779e9290f8e
FloreLaure <floresdg60@gmail.com>
2022-10-04 10:07:50 +0000

    modifié :         week4/day3/exercices/exerciceXp.py

--- week4/day3/exercices/exerciceXp.py
+++ week4/day3/exercices/exerciceXp.py
@@ -31,29 +31,73 @@ print("le total est ",tolal)
 
 ##  xercice3
 
+
+###   Create a dictionary called brand which value is the information from part one (turn the info into keys and values).
+
 brand = {
-    "name": "Zara" 
-    "creation_date": 1975 
-    "creator_name": ["Amancio", "Ortega", "Gaona" ]
-    "type_of_clothes": ["men", "women", "children", "home"] 
-    "international_competitors": ["Gap", "H&M", "Benetton" ]
-    "number_stores": 7000 
-    "major_color":{
-        "France": "blue" 
-        "Spain": "red" 
-        "US": ["pink", "green"]
+    'name': 'Zara',
+    'creation_date': 1975, 
+    'creator_name': ['Amancio', 'Ortega', 'Gaona'],
+    'type_of_clothes': ['men', 'women', 'children', 'home'], 
+    'international_competitors': ['Gap', 'H&M', 'Benetton' ],
+    'number_stores': 7000 ,
+    'major_color':{
+        'France': 'blue', 
+        'Spain': 'red', 
+        'US': ['pink', 'green']
         }
         
 }
-brand.update({"number_stores": 2})
-brand.append("country_creation":Spain)
-
-print(brand)
 
 
 
+brand.update({"number_stores": 2})  ## Change the number of stores to 2.
+
+print("les clients de Zara sont: ",brand['creator_name'])  ## Print a sentence that explains who Zaras clients are.
 
 
+brand['country_creation']='Spain' ##  Add a key called country_creation with a value of Spain.
+
+
+##    Check if the key international_competitors is in the dictionary. If it is, add the store Desigual.
+key='international_competitors'
+if (key in brand):
+    brand["international_competitors"].append("Desigual")
+
+
+
+##    Delete the information about the date of creation.
+del(brand["creation_date"])
+
+
+print((brand['international_competitors'])[-1])  ### Print the last international competitor.
+
+
+print((brand['major_color'])['US'])  ###  Print the major clothes colors in the US. 
+
+
+
+print(len(brand.items())) ### Print the amount of key value pairs (ie. length of the dictionary).
+ 
+
+print(brand.keys())  ###  Print the keys of the dictionary.
+
+
+
+####  Create another dictionary called more_on_zara
+
+more_on_Zara={
+    'creation_date': 1975,
+    'number_stores': 10000
+}
+
+
+
+brand.update(more_on_Zara) ###  Use a method to add the information from the dictionary more_on_zara to the dictionary brand. 
+
+
+
+print(brand["number_stores"]) ###  Print the value of the key number_stores
 
 
 
@@ -64,7 +108,7 @@ print(brand)
 
 users = ["Mickey","Minnie","Donald","Ariel","Pluto"]
 for item in enumerate(users):
-    print(list(item))
+    print(zip(item))
 
 
 
